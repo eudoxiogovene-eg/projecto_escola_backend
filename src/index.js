7b9f173d24a21f8267efc3453e580cec9eb846c5
eudoxio govene <eudoxiorafaelgovene35@gmail.com>
2023-04-25 15:01:05 +0100

    adicionando um array

--- src/index.js
+++ src/index.js
@@ -19,6 +19,23 @@ const infoGeral=require('./routes/informacoesGerais')
 
 
 const app=express()
+const dados=[
+    {nome:'eudoxio',sobrenome:'govene',idade:22,pais:'brasil',cidade:'sao paulo'},
+    {nome:'sousa',sobrenome:'rafael',idade:25,pais:'estados unidos',cidade:'nova york'},
+    {nome:'eugenio',sobrenome:'luxo',idade:27,pais:'alemanha',cidade:'berlim'},
+    {nome:'cipriano',sobrenome:'sitoe',idade:24,pais:'franca',cidade:'paris'},
+    {nome:'custodio',sobrenome:'cossa',idade:52,pais:'mocambique',cidade:'xai-xai'},
+    {nome:'fedula',sobrenome:'djeje',idade:24,pais:'italia',cidade:'roma'},
+    {nome:'shanaia',sobrenome:'tchaia',idade:12,pais:'portugal',cidade:'lisboa'},
+    {nome:'shanaia',sobrenome:'tchaia',idade:12,pais:'portugal',cidade:'lisboa'},
+    {nome:'shanaia',sobrenome:'tchaia',idade:12,pais:'portugal',cidade:'lisboa'},
+    {nome:'shanaia',sobrenome:'tchaia',idade:12,pais:'portugal',cidade:'lisboa'},
+    {nome:'shanaia',sobrenome:'tchaia',idade:12,pais:'portugal',cidade:'lisboa'},
+]
+
+app.get('/',(req,res)=>{
+    return res.status(200).json({message:'dados do usuario',dados})
+})
 const mongoose= require('mongoose')
 
 app.use(cors())
